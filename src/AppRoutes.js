93981081476abae14a2330631b5793238b4f930c
Pritipriya Singh <mail2pritipriya@gmail.com>
2023-07-25 03:17:51 +0530

    frontend done all

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from './Home';
-
+import Main from './component/Main';
+import Navbar from './component/Navbar';
+import Createteam from './component/CreateTeam';
 const AppRoutes = () => {
   return (
     <Router>
+       <Navbar/>
         <Routes>
-            <Route path='/' element={<Home />} />
+         
+          <Route path='/' element={<Main/>}></Route>
+            <Route path='/board' element={<Home />} />
+            <Route path='/group' element={<Createteam />} />
         </Routes>
     </Router>
   )
--- src/Home.jsx
+++ src/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import Container from './component/Container'
 
+
 const Home = () => {
   return (
     <div className='m-10'>
@@ -14,3 +15,4 @@ const Home = () => {
 }
 
 export default Home
+
