baba344e36bd888ff5a86f3e6d2bd5e97d9f627a
Pritipriya Singh <mail2pritipriya@gmail.com>
2023-07-25 21:23:43 +0530

    huddle01 added

--- src/component/Container.jsx
+++ src/component/Container.jsx
@@ -1,10 +1,11 @@
 import Board from "./Board";
+import CustomButton from "./CustomButtom";
 import React, { useState, useContext, useEffect} from "react";
 import { MyContext } from "../App";
 import { useHuddle01 } from '@huddle01/react';
 import { useLobby, useAudio, useVideo, useRoom, usePeers } from '@huddle01/react/hooks';
 import { useSigner, useAccount } from "wagmi";
-
+import { Audio, Video } from "@huddle01/react/components";
 function Container() {
     const [color, setColor] = useState("black");
     const { address } = useAccount();
@@ -12,28 +13,26 @@ function Container() {
       roomId,
       setRoomId
      } = useContext(MyContext);
-//      const { initialize, isInitialized } = useHuddle01();
-//      const { joinLobby } = useLobby();
-//      const { 
-//       Audio, fetchAudioStream, stopAudioStream, error: micError, 
-//        produceAudio, stopProducingAudio 
-//      } = useAudio();
-  
-//      const { 
-//        Video, fetchVideoStream, stopVideoStream, error: camError, 
-//        produceVideo, stopProducingVideo 
-//      } = useVideo(); 
-//      const { joinRoom, leaveRoom, camStream, micStream } = useRoom();
-//      const { peerIds } = usePeers();
-  
-// console.log(roomId)   
+     const { initialize, isInitialized } = useHuddle01();
+     const { joinLobby } = useLobby();
+     const { 
+      fetchAudioStream, stopAudioStream, error: micError, 
+      produceAudio, stopProducingAudio ,   stream: micStream,
+    } = useAudio();
+ 
+    const { 
+      fetchVideoStream, stopVideoStream, error: camError, 
+      produceVideo, stopProducingVideo ,   stream: camStream,
+    } = useVideo(); 
+     const { joinRoom, leaveRoom } = useRoom();
+     const { peers } = usePeers();
+ 
 
 
-// useEffect(() => {
-//   initialize("lxiHM4YZ52hb1tbgRTNkSDHZfNayJBpO")
-//   console.log()
-// }, []);
-
+useEffect(() => {
+  initialize("lxiHM4YZ52hb1tbgRTNkSDHZfNayJBpO")
+  console.log()
+}, []);
 
   return (
     <div className="conatiner mt-[7rem]">
@@ -43,61 +42,89 @@ function Container() {
       <div className="board-container">
           <Board color={color}></Board>
       </div>
-      {/* <div className="button">
+      <div className="button">
       <div className="grid grid-cols-4">
-          {peerIds.map(peer => (
-              <Video key={peer.peerId} peerId={peer.peerId} debug />
-          ))}
- 
-          {peerIds.map(peer => (
-              <Audio key={peer.peerId} peerId={peer.peerId} debug />
-          ))}
+      {Object.values(peers)
+            .filter((peer) => peer.cam)
+            .map((peer) => (
+              <>
+                role: {peer.role}
+                <Video
+                  key={peer.peerId}
+                  peerId={peer.peerId}
+                  track={peer.cam}
+                  debug
+                />
+              </>
+            ))}
+          {Object.values(peers)
+            .filter((peer) => peer.mic)
+            .map((peer) => (
+              <Audio key={peer.peerId} peerId={peer.peerId} track={peer.mic} />
+            ))}
         </div>
-      {
-        isInitialized ? 
+    
         <button 
-        disabled={joinLobby.isCallable} 
+        disabled={!joinLobby.isCallable}
+        className="border border-black p-2 rounded-md m-2"
         onClick={() => joinLobby(address)
+          
       }>
         Join Lobby
-      </button> : "Loadinggg.."
-      }
+      </button> 
+  
       </div>
 
    
         {/* Mic */} 
-        {/* <button disabled={!fetchAudioStream.isCallable} onClick={fetchAudioStream}>
+        <button disabled={!fetchAudioStream.isCallable} onClick={fetchAudioStream}
+        className="border border-black p-2 rounded-md m-2"
+        >
           FETCH_AUDIO_STREAM
-        </button> */}
+        </button>
  
         {/* Webcam */} 
-        {/* <button disabled={!fetchVideoStream.isCallable} onClick={fetchVideoStream}>
+        <button disabled={!fetchVideoStream.isCallable} 
+         className="border border-black p-2 rounded-md m-2"
+        onClick={fetchVideoStream}>
           FETCH_VIDEO_STREAM
         </button>
  
-        <button disabled={!joinRoom.isCallable} onClick={joinRoom}>
+        <button disabled={!joinRoom.isCallable}
+         className="border border-black p-2 rounded-md m-2"
+        onClick={joinRoom}>
           JOIN_ROOM 
         </button>
  
-        <button disabled={!leaveRoom.isCallable} onClick={leaveRoom}>
+        <button 
+         className="border border-black p-2 rounded-md m-2"
+        disabled={!leaveRoom.isCallable} onClick={leaveRoom}>
           LEAVE_ROOM 
         </button>
  
-        <button disabled={!produceVideo.isCallable} onClick={() => produceVideo(camStream)}>
+        <button 
+         className="border border-black p-2 rounded-md m-2"
+        disabled={!produceVideo.isCallable} onClick={() => produceVideo(camStream)}>
           Produce Cam  
         </button>
  
-        <button disabled={!produceAudio.isCallable} onClick={() => produceAudio(micStream)}>
+        <button 
+         className="border border-black p-2 rounded-md m-2"
+        disabled={!produceAudio.isCallable} onClick={() => produceAudio(micStream)}>
           Produce Mic  
         </button>
  
-        <button disabled={!stopProducingVideo.isCallable} onClick={stopProducingVideo}>
+        <button 
+         className="border border-black p-2 rounded-md m-2"
+        disabled={!stopProducingVideo.isCallable} onClick={stopProducingVideo}>
           Stop Producing Cam  
         </button>
  
-        <button disabled={!stopProducingAudio.isCallable} onClick={stopProducingAudio}>
+        <button
+         className="border border-black p-2 rounded-md m-2"
+        disabled={!stopProducingAudio.isCallable} onClick={stopProducingAudio}>
           Stop Producing Mic  
-        </button> */} 
+        </button>
     </div>
   );
 }
